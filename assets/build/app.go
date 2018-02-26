c41a7b070990bfce0e67d3e7ba1def6bbabe6da7
vebis <vebis@gmx.net>
2018-02-26 14:39:45 +0100

    use docker client

--- assets/build/app.go
+++ assets/build/app.go
@@ -1,73 +1,44 @@
 package main
 
 import (
+    "context"
     "os"
     "time"
     "log"
     "strconv"
-    "bufio"
     "io"
-    "os/exec"
-    "encoding/json"
-    "strings"
+
+    "github.com/docker/docker/api/types"
+    "github.com/docker/docker/api/types/events"
+    "github.com/docker/docker/api/types/filters"
+    "github.com/docker/docker/client"
 )
 
 var start_time int64
 var hm_until int
 var hm_delete_dangling int
-var hm_enforcing int
 var image_history = make(map[string]int64)
-var docker_socket string
 
-type Event struct {
-    From string `json:"from"`
-}
+var cli *client.Client
 
-type Image struct {
-    ID string   `json:"ID"`
-}
+var enforcing bool
 
 func getCurTimestamp() int64 {
     return int64(time.Now().Unix())
 }
 
-func cleanJson(s string) string {
-    return strings.Replace(s, "'", "", -1)
-}
-
-func getImageId(s string) string {
+func getImageId(image_name string) string {
     id := ""
 
-    cmd := exec.Command("docker", "image", "ls", s, "--format", "'{{json .}}'")
-    cmd.Stderr = os.Stderr
-    stdout, err := cmd.StdoutPipe()
-    if nil != err {
-        log.Fatalf("Error obtaining stdout: %s", err.Error())
+    image, bytes, err := cli.ImageInspectWithRaw(context.Background(), image_name)
+    if err != nil || len(bytes) == 0 {
+        return id
     }
-    reader := bufio.NewReader(stdout)
-    go func(reader io.Reader) {
-        scanner := bufio.NewScanner(reader)
-        for scanner.Scan() {
-            data := Image{}
-            err := json.Unmarshal([]byte(cleanJson(scanner.Text())), &data)
 
-            if err != nil {
-                log.Print(err.Error())
+    id = image.ID
 
-                return
-            }
-            log.Print(" found id of image: ", data.ID)
+    log.Printf("Image name '%s' mapped to image id '%s'", image_name, id)
 
-            id = data.ID
-        }
-    }(reader)
-
-    if err := cmd.Start(); nil != err {
-        log.Fatalf("Error starting program: %s, %s", cmd.Path, err.Error())
-    }
-    cmd.Wait()
-
-    log.Printf("[getImageId] image name '%s' mapped to image id '%s'", s, id)
     return id
 }
 
@@ -78,88 +49,67 @@ func updateImageHistory(image_id string) {
 }
 
 func checkForRunningContainer(image_id string, all bool) bool {
-    var container_ids []string
-    s := []string{"ancestor=", image_id}
-    cmd := exec.Command("docker", "ps", "-q", "--filter", strings.Join(s,""))
-    if all == true {
-        cmd = exec.Command("docker", "ps", "-a", "-q", "--filter", strings.Join(s,""))
-    }
-    cmd.Stderr = os.Stderr
-    stdout, err := cmd.StdoutPipe()
-    if nil != err {
-        log.Fatalf("Error obtaining stdout: %s", err.Error())
-    }
-    reader := bufio.NewReader(stdout)
-    go func(reader io.Reader) {
-        scanner := bufio.NewScanner(reader)
-        for scanner.Scan() {
-            container_ids = append(container_ids, scanner.Text())
-        }
-    }(reader)
+    filter := filters.NewArgs()
+    filter.Add("ancestor", image_id)
+    options := types.ContainerListOptions{Quiet: true, All: all, Filters : filter }
 
-    if err := cmd.Start(); nil != err {
-        log.Fatalf("Error starting program: %s, %s", cmd.Path, err.Error())
-    }
-    cmd.Wait()
+    containers, err := cli.ContainerList(context.Background(), options)
 
-    var ret bool
-    if len(container_ids) > 0 {
-        ret = true
-    } else {
+    ret := true // save default
+    if err != nil {
+        log.Print(err)
+
+        return ret
+    }
+
+    if len(containers) == 0 {
         ret = false
     }
 
-/*  DEBUG
-    log.Printf("[checkForRunningContainer] result for image id '%s' and flag all %s => %s", image_id, all, ret)
-*/
     return ret
 }
 
-func rmImage(image_id string) {
-    // check if not in use
-    if checkForRunningContainer(image_id, true) {
-        if hm_enforcing == 0 {
-            log.Print("Do nothing. Some containers are running or stopped!")
+func hasRunningContainers(image_id string) bool {
+    return checkForRunningContainer(image_id, false)
+}
 
-            return
-        } else {
-            log.Print("Some containers are running or stopped.")
-        }
+func hasStoppedContainers(image_id string) bool {
+    if checkForRunningContainer(image_id, false) == false && checkForRunningContainer(image_id, true) == true {
+	return true
     } else {
-        log.Print("Found no running containers. Proceeding...")
+        return false
     }
+}
 
-    // if in use by stopped container decide on HM_ENFORCING
-    if checkForRunningContainer(image_id, false) {
-        log.Print("Do nothing. Some containers are running!")
+func rmImage(image_id string) {
+    if hasRunningContainers(image_id) {
+        log.Print("    Some containers are running. Skipping!")
 
         return
     }
 
-    // delete image
-    log.Print("trying to delete image now")
-    cmd := exec.Command("docker", "image", "rm", "-f", image_id)
-    cmd.Stderr = os.Stderr
-    stdout, err := cmd.StdoutPipe()
-    if nil != err {
-        log.Fatalf("Error obtaining stdout: %s", err.Error())
-    }
-    reader := bufio.NewReader(stdout)
-    go func(reader io.Reader) {
-        scanner := bufio.NewScanner(reader)
-        for scanner.Scan() {
-            log.Print(scanner.Text())
-        }
-    }(reader)
+    if hasStoppedContainers(image_id) && !enforcing {
+        log.Print("    Some containers are stopped. Skipping!")
 
-    if err := cmd.Start(); nil != err {
-        log.Fatalf("Error starting program: %s, %s", cmd.Path, err.Error())
+        return
     }
-    cmd.Wait()
+
+    imagedeleteresponses, err := cli.ImageRemove(context.Background(), image_id, types.ImageRemoveOptions{Force:true})
+    if err != nil {
+      log.Print(err)
+    }
+
+    for _, response := range imagedeleteresponses {
+        if response.Deleted != "" {
+            log.Printf("    Deleted   : %s", response.Deleted)
+        } else {
+            log.Printf("    Untagged  : %s", response.Untagged)
+        }
+    }
 
     // remove image_id from image_history map
     if getImageId(image_id) == "" {
-        log.Print("Image seems to be deleted")
+        log.Print("  Image seems to be deleted")
         delete(image_history, image_id)
     }
 
@@ -173,8 +123,10 @@ func deleteOldImages() {
     for image_id, last := range image_history {
         log.Printf("  trying image: %s", image_id)
         if last < cur_time-int64(hm_until) {
-            log.Printf("Image %s is to old, deleting", image_id)
+            log.Print("    Image is to old, deleting.")
             rmImage(image_id)
+        } else {
+            log.Print("    Image is to new. Skipping.")
         }
     }
 
@@ -182,71 +134,44 @@ func deleteOldImages() {
 }
 
 func deleteGrandFatheredImages() {
+    log.Print("Trying to delete images without received start event")
     var image_id string
-    cmd := exec.Command("docker", "image", "ls", "--format", "'{{.ID}}'")
-    cmd.Stderr = os.Stderr
-    stdout, err := cmd.StdoutPipe()
-    if nil != err {
-        log.Fatalf("Error obtaining stdout: %s", err.Error())
-    }
-    reader := bufio.NewReader(stdout)
-    go func(reader io.Reader) {
-        scanner := bufio.NewScanner(reader)
-        for scanner.Scan() {
-            image_id = cleanJson(scanner.Text())
-            log.Print("  working on image: ", image_id)
-            if _, ok := image_history[image_id]; ok {
-                log.Print("Image is not grandfathered. Skipping!")
 
-                continue
-            } else {
-                rmImage(image_id)
-            }
+    images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+    if err != nil {
+        log.Print(err)
+
+        return
+    }
+
+    for _, image := range images {
+        image_id = image.ID
+        log.Print("  trying image: ", image_id)
+        if _, ok := image_history[image_id]; ok {
+            log.Print("    Image is not grandfathered. Skipping!")
+
+            continue
+        } else {
+            rmImage(image_id)
         }
-    }(reader)
-
-    if err := cmd.Start(); nil != err {
-        log.Fatalf("Error starting program: %s, %s", cmd.Path, err.Error())
     }
-    cmd.Wait()
 
     return
 }
 
 func deleteDanglingImages() {
     log.Print("Trying to clean dangled images")
-    cmd := exec.Command("docker", "image", "prune", "--force")
-    cmd.Stderr = os.Stderr
-    stdout, err := cmd.StdoutPipe()
-    if nil != err {
-        log.Fatalf("Error obtaining stdout: %s", err.Error())
-    }
-    reader := bufio.NewReader(stdout)
-    go func(reader io.Reader) {
-        scanner := bufio.NewScanner(reader)
-        for scanner.Scan() {
-            log.Print(scanner.Text())
-        }
-    }(reader)
 
-    if err := cmd.Start(); nil != err {
-        log.Fatalf("Error starting program: %s, %s", cmd.Path, err.Error())
+    prunereport, err := cli.ImagesPrune(context.Background(), filters.NewArgs())
+    if err != nil {
+        panic(err)
     }
-    cmd.Wait()
+
+    log.Print("  Space reclaimed: ", prunereport.SpaceReclaimed)
 }
 
-func handleEvent(s string) {
-    image_name := ""
-    data := Event{}
-    err := json.Unmarshal([]byte(cleanJson(s)), &data)
-    log.Print(" found event for image: ", data.From)
-    if err != nil {
-        log.Print(err.Error())
-
-        return
-    }
-
-    image_name = data.From
+func handleEvent(image_name string) {
+    log.Printf("Handle event for image '%s'",  image_name)
 
     image_id := getImageId(image_name)
 
@@ -264,8 +189,7 @@ func handleEvent(s string) {
         deleteDanglingImages()
     }
 
-    if getCurTimestamp()>start_time+int64(hm_until) && hm_enforcing == 1 {
-        log.Print("Trying to delete images without received start event")
+    if getCurTimestamp()>start_time+int64(hm_until) && enforcing {
         deleteGrandFatheredImages()
     }
 }
@@ -276,8 +200,8 @@ func main() {
     // set defaults
     hm_until = 7*24*60*60 // 1 week
     hm_delete_dangling = 1
-    hm_enforcing = 0
-    docker_socket = "/var/run/docker.sock"
+    hm_enforcing := 0
+    docker_socket := "/var/run/docker.sock"
 
     log.Print("Starting docker hausmeister ...")
 
@@ -313,6 +237,12 @@ func main() {
         }
     }
 
+    if hm_enforcing == 1 {
+        enforcing = true
+    } else {
+        enforcing = false
+    }
+
     log.Print("Configuration:")
     log.Print(" HM_UNTIL           : ", hm_until)
     log.Print(" HM_DELETE_DANGLING : ", hm_delete_dangling)
@@ -324,24 +254,30 @@ func main() {
         log.Print("Docker socket exists")
     }
 
-    cmd := exec.Command("docker", "events", "-f", "type=container", "-f", "event=create", "--format", "'{{json .}}'")
-    cmd.Stderr = os.Stderr
-    stdout, err := cmd.StdoutPipe()
-    if nil != err {
-        log.Fatalf("Error obtaining stdout: %s", err.Error())
+    clie, err := client.NewClientWithOpts(client.WithVersion("1.35"))
+    cli = clie
+    if err != nil {
+        panic(err)
     }
-    reader := bufio.NewReader(stdout)
-    go func(reader io.Reader) {
-        scanner := bufio.NewScanner(reader)
-        for scanner.Scan() {
-            handleEvent(scanner.Text())
-        }
-    }(reader)
 
-    if err := cmd.Start(); nil != err {
-        log.Fatalf("Error starting program: %s, %s", cmd.Path, err.Error())
-    }
-    cmd.Wait()
+    filter := filters.NewArgs()
+    filter.Add("type", events.ContainerEventType)
+    filter.Add("event", "create")
+    messages, errs := cli.Events(context.Background(), types.EventsOptions{Filters: filter})
+
+    loop :
+                for {
+                        select {
+                        case err := <-errs:
+                                if err != nil && err != io.EOF {
+                                        panic(err)
+                                }
+
+                                break loop
+                        case e := <-messages:
+                                        handleEvent(e.From)
+                        }
+                }
 
     log.Print("Stopped docker hausmeister")
 }
